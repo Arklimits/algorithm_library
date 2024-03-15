1ab8274dd7c4a8dff603fe3243defa7db47233c8
Arklimits <74225157+Arklimits@users.noreply.github.com>
2024-03-15 09:53:14 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
@@ -1,12 +1,13 @@
-c = int(input())
+if __name__ == '__main__':
+    C = int(input())
 
-for _ in range(c):
-    arr = list(map(int, input().split()))
-    avg = sum(arr[1:]) / arr[0]
+    for _ in range(C):
+        arr = list(map(int, input().split()))
+        avg = sum(arr[1:]) / arr[0]
 
-    cnt = 0
-    for i in arr[1:]:
-        if i > avg:
-            cnt += 1
+        cnt = 0
+        for i in arr[1:]:
+            if i > avg:
+                cnt += 1
 
-    print('{0:.3f}%'.format(cnt/arr[0]*100))
+        print("{0:.3f}%".format(cnt / arr[0] * 100))
