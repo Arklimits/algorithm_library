ae9366674696ab1f14572d394283731f7b72900c
Arklimits <74225157+Arklimits@users.noreply.github.com>
2024-03-15 17:47:13 +0900

    Update 카드 놓기.py

--- 백준/Silver/5568. 카드 놓기/카드 놓기.py
+++ 백준/Silver/5568. 카드 놓기/카드 놓기.py
@@ -1,47 +1,26 @@
+def combine(num, pick, on_hand):
+    if pick >= k:
+        if num not in combination:
+            combination.append(num)
+        return
+
+    for i in range(len(card)):
+        if i not in on_hand:
+            on_hand.append(i)
+            number = num + card[i]
+            combine(number, pick + 1, on_hand)
+            on_hand.pop()
+
+
 n = int(input())
 k = int(input())
+
 card = []
 combination = []
-count = 0
 
-for i in range(n):
-    card.append(int(input()))
+for _ in range(n):
+    card.append(input())
 
-if k == 2:
-    for i in range(len(card)):
-        for j in range(len(card)):
-            if j == i:
-                continue
-            temp = int(str(card[i]) + str(card[j]))
-            if temp not in combination:
-                combination.append(temp)
-                count += 1
-elif k == 3:
-    for i in range(len(card)):
-        for j in range(len(card)):
-            if j == i:
-                continue
-            for k in range(len(card)):
-                if k == i or k == j:
-                    continue
-                temp = int(str(card[i]) + str(card[j]) + str(card[k]))
-                if temp not in combination:
-                    combination.append(temp)
-                    count += 1
-else:
-    for i in range(len(card)):
-        for j in range(len(card)):
-            if j == i:
-                continue
-            for k in range(len(card)):
-                if k == i or k == j:
-                    continue
-                for m in range(len(card)):
-                    if m == i or m == j or m == k:
-                        continue
-                    temp = int(str(card[i]) + str(card[j]) + str(card[k]) + str(card[m]))
-                    if temp not in combination:
-                        combination.append(temp)
-                        count += 1
+combine("", 0, [])
 
-print(count)
+print(len(combination))
