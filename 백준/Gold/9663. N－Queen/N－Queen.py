18c04ea40e69e7e962e095ddfd093ddf379719d5
Arklimits <74225157+Arklimits@users.noreply.github.com>
2024-03-15 21:09:06 +0900

    [Gold IV] Title: N-Queen, Time: 23848 ms, Memory: 144120 KB -BaekjoonHub

---
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -0,0 +1,30 @@
+def check(value, arr):
+    length = len(arr)
+    for depth in range(1, length + 1):
+        if value - depth == arr[length - depth] or value + depth == arr[length - depth] or value in arr:
+            return False
+    return True
+
+
+def queen(now, hand):
+    global count
+
+    if now >= n:
+        count += 1
+        return
+
+    for i in range(n):
+        if i not in hand:
+            # print(i, hand)
+            if check(i, hand):
+                hand.append(i)
+                queen(now+1, hand)
+                hand.pop()
+
+
+n = int(input())
+count = 0
+
+queen(0, [])
+
+print(count)
