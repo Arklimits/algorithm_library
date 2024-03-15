b672e9a2be4f48a8e07d85d25402176d25a76af5
Arklimits <74225157+Arklimits@users.noreply.github.com>
2024-03-15 10:22:01 +0900

    [Bronze II] Title: 문자열 반복, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.py
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.py
@@ -1,8 +1,9 @@
-t = int(input())
+if __name__ == '__main__':
+    t = int(input())
 
-for i in range(t):
-    r, s = map(str, input().split())
-    for k in range(len(s)):
-        for j in range(int(r)):
-            print(s[k], end='')
-    print()
+    for _ in range(t):
+        r, s = input().split()
+        for i in range(len(s)):
+            for j in range(int(r)):
+                print(s[i], end='')
+        print()
