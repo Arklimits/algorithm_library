90782a9c45b28b171bd260b6a8bde58e15b43a74
Arklimits <74225157+Arklimits@users.noreply.github.com>
2024-03-16 13:19:42 +0900

    [Silver II] Title: 외판원 순회 2, Time: 1808 ms, Memory: 113908 KB -BaekjoonHub

---
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
@@ -0,0 +1,40 @@
+import sys
+
+
+def travel(depth, num, hand):
+    global ans, n, pivot
+
+    if depth == 0:
+        price = 0
+
+        if a[hand[-1]][hand[0]] != 0:
+            hand.append(hand[0])
+            for p in range(n):
+                price += a[hand[p]][hand[p + 1]]
+
+            hand.pop()
+
+            if price < ans or pivot:
+                ans = price
+                pivot = 0
+                return
+    else:
+        for i in range(n):
+            if a[num - 1][i] != 0:
+                if i not in hand and (a[num-1][i] < ans or pivot):
+                    hand.append(i)
+                    travel(depth - 1, i + 1, hand)
+                    hand.pop()
+
+
+a = []
+ans = 0
+pivot = 1
+n = int(sys.stdin.readline())
+
+for t in range(n):
+    a.append(list(map(int, input().split())))
+
+travel(n, 0, [])
+
+print(ans)
