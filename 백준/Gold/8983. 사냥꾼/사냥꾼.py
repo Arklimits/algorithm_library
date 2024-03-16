15bcd17d17c84e8f63196aa1099eaa96cffc3ecb
Arklimits <74225157+Arklimits@users.noreply.github.com>
2024-03-16 22:35:24 +0900

    [Gold IV] Title: 사냥꾼, Time: 824 ms, Memory: 54880 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/8983. 사냥꾼/사냥꾼.py
+++ 백준/Gold/8983. 사냥꾼/사냥꾼.py
@@ -1,28 +1,34 @@
 import sys
 
-limit = 1000000000
-animal = []
-shoot = []
 count = 0
 
 m, n, L = map(int, sys.stdin.readline().split())
 x = list(map(int, sys.stdin.readline().split()))
-for _ in range(n):
-    animal.append(list(map(int, sys.stdin.readline().split())))
+animal = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
 
 x.sort()
 
 for loc, dist in animal:
+    start, end = 0, len(x) - 1
+    left_shoot = loc + dist - L
+    right_shoot = loc - (dist - L)
+
     if dist > L:
         continue
     elif dist == L:
         if loc in x:
             count += 1
+        continue
     else:
-        pivot = L - dist
-        for i in range(pivot):
-            if loc - i - 1 in x or loc + i + 1 in x:
+        while start <= end:
+            mid = (start + end) // 2
+
+            if left_shoot <= x[mid] <= right_shoot:
                 count += 1
                 break
+            elif left_shoot > x[mid]:
+                start = mid + 1
+            elif right_shoot < x[mid]:
+                end = mid - 1
 
 print(count)
