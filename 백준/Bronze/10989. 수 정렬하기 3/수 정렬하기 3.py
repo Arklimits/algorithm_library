40496cc32d30d118ef81a2d3e41837c87f395ff6
Arklimits <74225157+Arklimits@users.noreply.github.com>
2024-03-16 09:48:39 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 9388 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -0,0 +1,14 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+arr = [0] * 10001
+
+for i in range(n):
+    a = int(sys.stdin.readline())
+    arr[a] += 1
+
+for i in range(len(arr)):
+    if arr[i]:
+        for j in range(arr[i]):
+            print(i)
