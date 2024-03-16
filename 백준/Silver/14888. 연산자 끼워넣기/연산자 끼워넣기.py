9e1891844f961547019fe2415a521910cde0f9e2
Arklimits <74225157+Arklimits@users.noreply.github.com>
2024-03-16 14:30:54 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 60 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,16 +1,36 @@
 import sys
-from itertools import permutations
 
 
-def calculator(opr, num1, num2):
-    if opr == 0:
-        return num1 + num2
-    elif opr == 1:
-        return num1 - num2
-    elif opr == 2:
-        return num1 * num2
+def calculator(i, num):
+    global maximum, minimum, pivot
+
+    if i == n:
+        if pivot:
+            maximum = num
+            minimum = num
+            pivot = 0
+        elif num > maximum:
+            maximum = num
+        elif num < minimum:
+            minimum = num
+
     else:
-        return int(num1 / num2)
+        if y[0] > 0:
+            y[0] -= 1
+            calculator(i + 1, num + a[i])
+            y[0] += 1
+        if y[1] > 0:
+            y[1] -= 1
+            calculator(i + 1, num - a[i])
+            y[1] += 1
+        if y[2] > 0:
+            y[2] -= 1
+            calculator(i + 1, num * a[i])
+            y[2] += 1
+        if y[3] > 0:
+            y[3] -= 1
+            calculator(i + 1, int(num / a[i]))
+            y[3] += 1
 
 
 pivot = 1
@@ -19,26 +39,8 @@ maximum = minimum = 0
 n = int(sys.stdin.readline())
 a = list(map(int, input().split()))
 y = list(map(int, input().split()))
-stack = []
 
-for i in range(4):
-    for j in range(y[i]):
-        stack.append(i)
-
-oper = list(permutations(stack))
-
-for j in range(len(oper)):
-    value = a[0]
-    for i in range(1, n):
-        value = calculator(oper[j][i-1], value, a[i])
-
-    if pivot:
-        maximum = minimum = value
-        pivot = 0
-    elif value > maximum:
-        maximum = value
-    elif value < minimum:
-        minimum = value
+calculator(1, a[0])
 
 print(maximum)
 print(minimum)
