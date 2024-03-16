68a54f39179f690b482adef867aaf3b1c752bb52
Arklimits <74225157+Arklimits@users.noreply.github.com>
2024-03-16 13:37:59 +0900

    [Silver II] Title: 외판원 순회 2, Time: 1668 ms, Memory: 113296 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
@@ -1,30 +1,38 @@
 import sys
 
 
-def travel(depth, num, hand):
+def travel(depth, num, hand, price):
     global ans, n, pivot
 
-    if depth == 0:
-        price = 0
+    if depth == n:
+        for i in range(n):
+            if i not in hand and (a[num - 1][i] < ans or pivot):
+                hand.append(i)
 
+                travel(depth - 1, i + 1, hand, 0)
+
+                hand.pop()
+
+    elif depth == 0:
         if a[hand[-1]][hand[0]] != 0:
-            for p in range(n-1):
-                price += a[hand[p]][hand[p + 1]]
-                if price > ans and not pivot:
-                    return
-                
+            if price > ans and not pivot:
+                return
+
             price += a[hand[-1]][hand[0]]
 
             if price < ans or pivot:
                 ans = price
                 pivot = 0
                 return
+
     else:
         for i in range(n):
             if a[num - 1][i] != 0:
                 if i not in hand and (a[num-1][i] < ans or pivot):
                     hand.append(i)
-                    travel(depth - 1, i + 1, hand)
+
+                    travel(depth - 1, i + 1, hand, price + a[num - 1][i])
+
                     hand.pop()
 
 
@@ -36,6 +44,6 @@ n = int(sys.stdin.readline())
 for t in range(n):
     a.append(list(map(int, input().split())))
 
-travel(n, 0, [])
+travel(n, 0, [], 0)
 
 print(ans)
