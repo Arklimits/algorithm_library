4011b72cc38ee4061db55227904aaf4423a7d554
Arklimits <74225157+Arklimits@users.noreply.github.com>
2024-03-18 20:52:32 +0900

    [Gold IV] Title: N-Queen, Time: 6088 ms, Memory: 141620 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,6 +1,7 @@
 def queen(now, basket):
     global count
-
+    pivot = 0
+    
     if now >= n:
         count += 1
         return
@@ -8,14 +9,13 @@ def queen(now, basket):
     for i in range(n - now):
         pivot = basket[0]
         del basket[0]
-
         if not up[now + pivot] and not down[now - pivot]:
+            
             up[now + pivot] = down[now - pivot] = 1
 
             queen(now + 1, basket)
 
             up[now + pivot] = down[now - pivot] = 0
-
         basket.append(pivot)
 
     return
