73a104839889f3862bb9fc1d1a62d2a9a3c9e75e
Arklimits <74225157+Arklimits@users.noreply.github.com>
2024-03-18 17:08:20 +0900

    [Gold IV] Title: N-Queen, Time: 7772 ms, Memory: 147672 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,26 +1,24 @@
-def queen(now, hand, handup, handdown, basket):
+def queen(now, basket):
     global count
 
     if now >= n:
         count += 1
         return
 
-    for i in range(len(basket)):
+    for i in range(n - now):
         pivot = basket[0]
         basket.remove(pivot)
-        
-        if pivot not in hand:
-            if now + pivot not in handup and now - pivot not in handdown:
-                hand.append(pivot)
-                handup.append(now + pivot)
-                handdown.append(now - pivot)
 
-                queen(now + 1, hand, handup, handdown, basket)
+        if not hand[pivot]:
+            if not up[now + pivot] and not down[now - pivot]:
+                hand[pivot] = 1
+                up[now + pivot] = down[now - pivot] = 1
 
-                hand.pop()
-                handup.pop()
-                handdown.pop()
-                
+                queen(now + 1, basket)
+
+                hand[pivot] = 0
+                up[now + pivot] = down[now - pivot] = 0
+
         basket.append(pivot)
 
     return
@@ -29,8 +27,12 @@ def queen(now, hand, handup, handdown, basket):
 n = int(input())
 count = 0
 
+hand = [0] * n
+up = [0] * n * 2
+down = [0] * n * 2
+
 arr = [i for i in range(n)]
 
-queen(0, [], [], [], arr)
+queen(0, arr)
 
 print(count)
