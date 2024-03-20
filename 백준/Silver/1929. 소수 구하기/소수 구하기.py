65ec2960cec8753a20ef065141c9c458f950cd1c
Arklimits <74225157+Arklimits@users.noreply.github.com>
2024-03-20 19:22:34 +0900

    [Silver III] Title: 소수 구하기, Time: 300 ms, Memory: 38792 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,11 +1,13 @@
-m, n = map(int, input().split())
+import sys
+
+m, n = map(int, sys.stdin.readline().split())
 arr = [1] * (n + 1)
 arr[0] = arr[1] = 0
 
 for i in range(2, int(n**0.5) + 1):
-    for j in range(i+i, n+1, i):
-        arr[j] = 0
-
+    if arr[i]:
+        for j in range(i+i, n+1, i):
+            arr[j] = 0
 
 for i in range(m, n+1):
     if arr[i]:
