2767ba0b81e757b01a1c836f7d1d97d589a2966c
Arklimits <74225157+Arklimits@users.noreply.github.com>
2024-03-20 19:19:29 +0900

    [Silver III] Title: 소수 구하기, Time: 624 ms, Memory: 38792 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,23 +1,12 @@
 m, n = map(int, input().split())
-arr = []
+arr = [1] * (n + 1)
+arr[0] = arr[1] = 0
 
-for i in range(2, n+1):
-    pivot = 1
+for i in range(2, int(n**0.5) + 1):
+    for j in range(i+i, n+1, i):
+        arr[j] = 0
 
-    for j in arr:
-        if i % j == 0:
-            pivot = 0
-            break
-        if j**2 > i:
-            break
 
-    if pivot:
-        arr.append(i)
-
-for i in range(len(arr)):
-    if arr[i] >= m:
-        check = i
-        break
-
-for i in range(check, len(arr)):
-    print(arr[i])
+for i in range(m, n+1):
+    if arr[i]:
+        print(i)
