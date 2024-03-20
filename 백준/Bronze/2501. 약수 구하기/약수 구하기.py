9a7e8c8b1b502414a32e5f7f7e8cc80b8cdcb026
Arklimits <74225157+Arklimits@users.noreply.github.com>
2024-03-20 19:25:44 +0900

    [Bronze III] Title: 약수 구하기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.py
@@ -0,0 +1,15 @@
+n, k = map(int, input().split())
+arr = []
+
+for i in range(1, n//2):
+    if not n % i:
+        arr.append(i)
+        arr.append(n//i)
+
+arr = list(set(arr))
+arr.sort()
+
+if k > len(arr):
+    print(0)
+else:
+    print(arr[k-1])
