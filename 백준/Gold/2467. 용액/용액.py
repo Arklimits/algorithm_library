bf49127965597bd80c77d23f2d1b374a5a35e39e
Arklimits <74225157+Arklimits@users.noreply.github.com>
2024-03-20 22:35:37 +0900

    [Gold V] Title: 용액, Time: 584 ms, Memory: 42036 KB -BaekjoonHub

---
+++ 백준/Gold/2467. 용액/용액.py
@@ -0,0 +1,33 @@
+import sys
+
+
+read = sys.stdin.readline
+n = int(read())
+arr = list(map(int, read().split()))
+
+lp, rp = 0, n
+res = 2000000000
+
+for i in range(n):
+    check = n
+    start, end = i+1, n-1
+
+    while start <= end:
+        mid = (start + end) // 2
+        if arr[mid] >= -arr[i]:
+            check = mid
+            end = mid - 1
+        else:
+            start = mid + 1
+
+    if i + 1 <= check < n:
+        if abs(arr[i] + arr[check]) < res:
+            res = abs(arr[i] + arr[check])
+            lp, rp = arr[i], arr[check]
+            
+    if i + 1 <= check - 1 < n:
+        if abs(arr[i] + arr[check - 1]) < res:
+            res = abs(arr[i] + arr[check - 1])
+            lp, rp = arr[i], arr[check - 1]
+
+print(lp, rp)
