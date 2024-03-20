65e67a649a6ed1b22f52ed33454e8eb5719c8524
Arklimits <74225157+Arklimits@users.noreply.github.com>
2024-03-20 23:02:43 +0900

    [Bronze II] Title: 분해합, Time: 1016 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -1,14 +1,18 @@
 n = int(input())
 
-for i in range(n):
-    text = str(i)
-    plus = 0
 
-    for j in text:
-        plus += int(j)
+def function():
+    for i in range(n):
+        text = str(i)
+        plus = 0
+    
+        for j in text:
+            plus += int(j)
+    
+        if (i + plus) == n:
+            print(i)
+            exit()
 
-    if (i + plus) == n:
-        print(i)
-        exit()
+    print(0)
 
-print(0)
+function()
