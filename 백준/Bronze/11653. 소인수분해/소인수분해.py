a5d63dd99d53d895d2ab8f6044d35421d08a7bd3
Arklimits <74225157+Arklimits@users.noreply.github.com>
2024-03-20 20:53:45 +0900

    [Bronze I] Title: 소인수분해, Time: 1564 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.py
+++ 백준/Bronze/11653. 소인수분해/소인수분해.py
@@ -1,26 +1,13 @@
-n = int(input())
-if n == 1:
-    exit()
+import sys
 
-arr = [0] * 2 + [1] * (n - 1)
 
-for i in range(2, int(n**0.5)+1):
-    if arr[i]:
-        for j in range(i+i, n+1, i):
-            arr[j] = 0
+n = int(sys.stdin.readline())
+i = 2
 
-if arr[n]:
-    print(n)
-    exit()
+while n >= i:
+    if n % i == 0:
+        print(i)
+        n /= i
+    else:
+        i += 1
 
-dummy = n
-while 1:
-    for i in range(2, n+1):
-        if arr[i]:
-            if not dummy % i:
-                print(i)
-                dummy //= i
-                break
-
-    if dummy == 1:
-        break
