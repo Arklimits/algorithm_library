df84d5ef49677155cfacae390c807a34cb3ca469
Arklimits <74225157+Arklimits@users.noreply.github.com>
2024-03-21 17:05:03 +0900

    [Gold IV] Title: N-Queen, Time: 18220 ms, Memory: 34060 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,16 +1,16 @@
+from collections import deque
+
+
 def queen(now, basket):
     global count
-    pivot = 0
-    
+
     if now >= n:
         count += 1
         return
 
     for i in range(n - now):
-        pivot = basket[0]
-        del basket[0]
+        pivot = basket.popleft()
         if not up[now + pivot] and not down[now - pivot]:
-            
             up[now + pivot] = down[now - pivot] = 1
 
             queen(now + 1, basket)
@@ -28,6 +28,7 @@ up = [0] * (n * 2 - 1)
 down = [0] * (n * 2 - 1)
 
 arr = [i for i in range(n)]
+arr = deque(arr)
 
 queen(0, arr)
 
