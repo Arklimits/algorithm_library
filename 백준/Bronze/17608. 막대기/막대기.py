c521bda99324c94a80965c9a096edf8bc74aaf07
Arklimits <74225157+Arklimits@users.noreply.github.com>
2024-03-21 15:19:11 +0900

    [Bronze II] Title: 막대기, Time: 76 ms, Memory: 34972 KB -BaekjoonHub

--- 백준/Bronze/17608. 막대기/막대기.py
+++ 백준/Bronze/17608. 막대기/막대기.py
@@ -5,20 +5,25 @@ read = sys.stdin.readline
 n = int(read())
 stack = []
 
-for _ in range(n):
-    stack.append(int(read()))
 
-high = max(stack)
-right = stack.pop()
-cnt = 1
+def program():
+    for _ in range(n):
+        stack.append(int(read()))
+    
+    high = max(stack)
+    right = stack.pop()
+    cnt = 1
+    
+    for _ in reversed(range(n-1)):
+        temp = stack.pop()
+    
+        if temp > right:
+            cnt += 1
+            right = temp
+        if temp == high:
+            break
+    
+    print(cnt)
+    
 
-for i in reversed(range(n-1)):
-    temp = stack.pop()
-
-    if temp > right:
-        cnt += 1
-        right = temp
-    if temp == high:
-        break
-
-print(cnt)
+program()
