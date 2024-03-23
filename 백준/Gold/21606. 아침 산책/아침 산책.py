a8dcc5a184a8a271de8bfef51dab5949e2ba9103
Arklimits <74225157+Arklimits@users.noreply.github.com>
2024-03-23 20:48:04 +0900

    [Gold III] Title: 아침 산책, Time: 68 ms, Memory: 31120 KB, Score: 60 point -BaekjoonHub

---
+++ 백준/Gold/21606. 아침 산책/아침 산책.py
@@ -0,0 +1,45 @@
+import sys
+
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+def dfs(start, pivot, visit):
+    global CNT
+
+    # print(f"NOW IN: {pivot=}")
+    if start != pivot:
+        if PLACE[pivot]:
+            CNT += 1
+            return
+
+    visit[pivot] = 1
+
+    for i in GRAPH[pivot]:
+        if not visit[i]:
+            dfs(start, i, visit)
+
+def program():  # 구동부
+    for i in range(N):
+        visit = [0 for _ in range(N)]
+        if PLACE[i]:
+            # print(f"START {i=}")
+            dfs(i, i, visit)
+
+    print(CNT)
+
+
+# 선언부
+N = int(input())
+CNT = 0
+GRAPH = [[] for _ in range(N)]
+PLACE = list(map(int, input().strip()))
+
+for __ in range(N-1):
+    F, T = map(int, input().split())
+    GRAPH[F-1].append(T-1)
+    GRAPH[T-1].append(F-1)
+
+for __ in range(N):
+    GRAPH[__].sort()
+
+program()
