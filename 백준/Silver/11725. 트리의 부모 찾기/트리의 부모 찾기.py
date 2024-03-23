faaf5e4dadde7e4fdea99f0c4016bbb2eacf370e
Arklimits <74225157+Arklimits@users.noreply.github.com>
2024-03-23 19:32:19 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 420 ms, Memory: 65708 KB -BaekjoonHub

---
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -0,0 +1,36 @@
+import sys
+
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+def dfs(pivot):
+    if VISIT[pivot]:
+        return
+    VISIT[pivot] = 1
+    for i in ARR[pivot]:
+        if not PAR[i]:
+            PAR[i] = pivot
+        dfs(i)
+
+def program():  # 구동부
+    dfs(1)
+
+    for i in range(2, N+1):
+        print(PAR[i])
+
+
+# 선언부
+N = int(input())
+ARR = [[] for _ in range(N+1)]
+PAR = [0 for _ in range(N+1)]
+VISIT = [0 for _ in range(N+1)]
+
+for _ in range(N-1):
+    F, T = map(int, input().split())
+    ARR[F].append(T)
+    ARR[T].append(F)
+
+for _ in range(N):
+    ARR[_].sort()
+
+program()
