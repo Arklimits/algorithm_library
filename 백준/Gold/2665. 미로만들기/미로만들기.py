442a840242a76c27e01ec3df26aad45c05fbf9d0
Arklimits <74225157+Arklimits@users.noreply.github.com>
2024-03-24 13:57:05 +0900

    [Gold IV] Title: 미로만들기, Time: 52 ms, Memory: 33188 KB -BaekjoonHub

--- 백준/Gold/2665. 미로만들기/미로만들기.py
+++ 백준/Gold/2665. 미로만들기/미로만들기.py
@@ -5,12 +5,10 @@ sys.setrecursionlimit(10**4)
 input = sys.stdin.readline
 
 def bfs(graph, y_s, x_s, y_e, x_e):
-    visit = [[0] * N for _ in range(N)]
-    res = [[25000001] * N for _ in range(N)]
+    res = [[2501] * N for _ in range(N)]
     queue = []
     heappush(queue, (0, (y_s, x_s)))
 
-    visit[y_s][x_s] = 1
     res[y_s][x_s] = 0
 
     while queue:
@@ -21,21 +19,23 @@ def bfs(graph, y_s, x_s, y_e, x_e):
 
         for i in range(4):
             dy, dx = y+DY[i], x+DX[i]
-            if 0 <= dy < N and 0 <= dx < N and not visit[dy][dx]:
+            if 0 <= dy < N and 0 <= dx < N:
                 val = cost + graph[dy][dx]
                 if val < res[dy][dx]:
-                    visit[dy][dx] = 1
                     res[dy][dx] = val
                     heappush(queue, (val, (y+DY[i], x+DX[i])))
 
-    print(res[y_e][x_e] // 10000)
+    print(res[y_e][x_e])
 
 
 def program(arr):
     for i in range(N):
         for j in range(N):
             if not arr[i][j]:
-                arr[i][j] = 10000
+                arr[i][j] = 1
+            else:
+                arr[i][j] = 0
+
 
     bfs(arr, 0, 0, N-1, N-1)
 
