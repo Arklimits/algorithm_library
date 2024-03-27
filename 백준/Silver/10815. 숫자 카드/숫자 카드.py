fe3253293d0968edfd9056896ca28a649b70febb
Arklimits <74225157+Arklimits@users.noreply.github.com>
2024-03-27 16:04:57 +0900

    [Silver V] Title: 숫자 카드, Time: 708 ms, Memory: 125632 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -2,28 +2,12 @@ import sys
 
 if __name__ == '__main__':
     n = int(sys.stdin.readline())
-    arr = []
-    n_arr = list(map(int, sys.stdin.readline().split()))
+    n_arr = set(map(int, sys.stdin.readline().split()))
     m = int(sys.stdin.readline())
     m_arr = list(map(int, sys.stdin.readline().split()))
 
-    n_arr.sort()
-
-    for i in range(m):
-        start = 0
-        end = n-1
-        while start <= end:
-            mid = (start + end) // 2
-            if n_arr[mid] == m_arr[i]:
-                print(1, end=' ')
-                break
-            elif n_arr[mid] < m_arr[i]:
-                start = mid + 1
-            else:
-                end = mid - 1
+    for i in m_arr:
+        if i in n_arr:
+            print(1, end=' ')
         else:
-            if n_arr[end] == m_arr[i]:
-                print(1, end=' ')
-            else:
-                print(0, end=' ')
-
+            print(0, end=' ')
