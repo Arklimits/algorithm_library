300cab2bcafd348f83f9de5e285baa01cc912f02
Arklimits <74225157+Arklimits@users.noreply.github.com>
2024-03-27 23:27:23 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 7432 ms, Memory: 294376 KB -BaekjoonHub

---
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.py
@@ -0,0 +1,40 @@
+import sys
+import heapq
+
+if __name__ == '__main__':
+    n = int(sys.stdin.readline())
+
+    for _ in range(n):
+        queue1 = []
+        queue2 = []
+        x = int(sys.stdin.readline())
+        id_check = [1] * x
+
+        for i in range(x):
+            comm, num = sys.stdin.readline().split()
+            num = int(num)
+
+            if comm == 'I':
+                heapq.heappush(queue1, (-num, i))
+                heapq.heappush(queue2, (num, i))
+            else:
+                if num == 1:
+                    if queue1:
+                        temp = heapq.heappop(queue1)
+                        id_check[temp[1]] = 0
+                if num == -1:
+                    if queue2:
+                        temp = heapq.heappop(queue2)
+                        id_check[temp[1]] = 0
+
+            while queue1 and not id_check[queue1[0][1]]:
+                heapq.heappop(queue1)
+            while queue2 and not id_check[queue2[0][1]]:
+                heapq.heappop(queue2)
+
+        if queue1 and queue2:
+            num1, i = heapq.heappop(queue1)
+            num2, j = heapq.heappop(queue2)
+            print(-num1, num2)
+        else:
+            print('EMPTY')
