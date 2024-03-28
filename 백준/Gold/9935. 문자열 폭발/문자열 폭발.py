1e64f742e3a25e8f379298163a2ada42872a8c5c
Arklimits <74225157+Arklimits@users.noreply.github.com>
2024-03-28 10:36:58 +0900

    [Gold IV] Title: 문자열 폭발, Time: 636 ms, Memory: 56896 KB -BaekjoonHub

---
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -0,0 +1,32 @@
+import sys
+from collections import deque
+
+if __name__ == '__main__':
+    text = list(map(str, sys.stdin.readline().strip()))
+    explosive = sys.stdin.readline().strip()
+    stack = deque()
+
+    explosive = list(reversed(explosive))
+
+    for i in text:
+        if i == explosive[0]:
+            temp = deque()
+            for j in range(1, len(explosive)):
+                if stack:
+                    if stack[-1] == explosive[j]:
+                        temp.append(stack.pop())
+                    else:
+                        stack += reversed(temp)
+                        break
+                else:
+                    stack += reversed(temp)
+                    break
+            else:
+                continue
+
+        stack.append(i)
+
+    if stack:
+        print(''.join(stack))
+    else:
+        print('FRULA')
