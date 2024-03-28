3ee8ccab8e1dbf25768b2893c047ba2ee4a3737d
Arklimits <74225157+Arklimits@users.noreply.github.com>
2024-03-28 16:32:42 +0900

    [Gold IV] Title: 동전 바꿔주기, Time: 244 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/2624. 동전 바꿔주기/동전 바꿔주기.py
@@ -0,0 +1,28 @@
+import sys
+
+
+def exchange():
+    for coin, count in ARR:
+        for i in reversed(range(T+1)):
+            for j in range(1, count + 1):
+                if i < coin * j:
+                    break
+                DP[i] += DP[i - coin * j]
+
+    if DP[T] < int(sys.maxsize):
+        print(DP[T])
+    else:
+        print(0)
+
+
+if __name__ == '__main__':
+    T = int(sys.stdin.readline())
+    K = int(sys.stdin.readline())
+    ARR = []
+    DP = [1] + [0 for _ in range(T)]
+    for _ in range(K):
+        ARR.append(list(map(int, sys.stdin.readline().split())))
+
+    ARR.sort(key=lambda x: -x[0])
+
+    exchange()
