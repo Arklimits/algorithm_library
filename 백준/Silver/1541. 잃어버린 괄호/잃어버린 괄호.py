14209279840af001dafa80dc52040e171e08877d
Arklimits <74225157+Arklimits@users.noreply.github.com>
2024-03-30 13:06:19 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 68 ms, Memory: 34192 KB -BaekjoonHub

---
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -0,0 +1,43 @@
+import sys
+from collections import deque
+
+
+def greedy():
+    number = ['' for _ in range(25)]
+    operator = deque()
+
+    n = 0
+    while TEXT:
+        target = TEXT.popleft()
+
+        if '0' <= target <= '9':
+            number[n] += target
+        else:
+            n += 1
+            operator.append(target)
+
+    for i in range(25):
+        if number[i] == '':
+            number[i] = 0
+        else:
+            # noinspection PyTypeChecker
+            number[i] = int(number[i])
+
+    idx = 0
+    while operator:
+        oper = operator.popleft()
+        idx += 1
+        if oper == '-':
+            number[idx] = -abs(number[idx])
+            while operator:
+                operator.popleft()
+                idx += 1
+                number[idx] = -abs(number[idx])
+    else:
+        print(sum(number))
+
+
+if __name__ == '__main__':
+    TEXT = deque(list(map(str, sys.stdin.readline().strip().rstrip())))
+
+    greedy()
