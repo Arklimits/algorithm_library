fdee568d620537f3ef1237800ad83e113504e773
Arklimits <74225157+Arklimits@users.noreply.github.com>
2024-04-02 20:23:40 +0900

    [Gold V] Title: 공통 부분 문자열, Time: 2416 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/5582. 공통 부분 문자열/공통 부분 문자열.py
+++ 백준/Gold/5582. 공통 부분 문자열/공통 부분 문자열.py
@@ -2,23 +2,24 @@ import sys
 
 
 def dynamic(text1, text2):
-    dp = [[0 for _ in range(len(text2) + 1)] for _ in range(len(text1) + 1)]
+    if len(text1) > len(text2):
+        text1, text2 = text2, text1
+
     max_length = 0
 
-    for i in range(1, len(text1) + 1):
-        for j in range(1, len(text2) + 1):
-            if text1[i-1] == text2[j-1]:
-                dp[i][j] = dp[i - 1][j - 1] + 1
-                max_length = max(max_length, dp[i][j])
-
-    # for i in range(len(text1) + 1):
-    #     print(*dp[i])
+    j = 0
+    for i in range(1, len(text1)+1):
+        # print(text1[j:i])
+        if text1[j:i] in text2:
+            max_length = max(max_length, i-j)
+        else:
+            j += 1
 
     print(max_length)
 
 
 if __name__ == '__main__':
-    TEXT1 = list(map(str, sys.stdin.readline().strip()))
-    TEXT2 = list(map(str, sys.stdin.readline().strip()))
+    TEXT1 = sys.stdin.readline().rstrip()
+    TEXT2 = sys.stdin.readline().rstrip()
 
     dynamic(TEXT1, TEXT2)
