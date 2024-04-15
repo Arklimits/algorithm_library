70ad54f1e199a75d3d03b2e4bac207f90b6b07a8
Arklimits <74225157+Arklimits@users.noreply.github.com>
2024-04-15 14:57:08 +0900

    [Silver II] Title: 랜선 자르기, Time: 4 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int k, n, i;
+    long end = 0;
+
+    scanf("%d %d", &k, &n);
+    long arr[k];
+
+    for (i = 0; i < k; i++) {
+        scanf("%ld", &arr[i]);
+        end += arr[i];
+    }
+    end /= n;
+    long start = 1, mid = 0, val = 0;
+    long res = 0;
+    while (start <= end) {
+        mid = (start + end) / 2;
+        val = 0;
+        for (i = 0; i < k; i++)
+            val += arr[i] / mid;
+
+        if (val >= n) {
+            if (res < mid)
+                res = mid;
+            start = mid + 1;
+        } else
+            end = mid - 1;
+    }
+
+    printf("%ld\n", res);
+
+    return 0;
+}
